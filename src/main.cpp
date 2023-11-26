feb22f3d002a1aa991a783ffbcaeedf7029e5a54
Yaroslav <yabaranovst@gmail.com>
2023-11-26 14:39:23 +0700

    New report lab_1

--- src/main.cpp
+++ src/main.cpp
@@ -9,55 +9,6 @@ int main()
 
 	grid.ReadGridJSON();
 	grid.CreateGrid();
-
-	auto numEdge = grid.GetNumberEdge(Grid::Edge{ {275, 276, 282}, {0, -1} });
-	
-	std::cout << numEdge;
-	std::cout << std::endl << std::endl;
-
-	auto numsEdges = grid.GetNumbersEdges(915);
-
-	for(const auto& num: numsEdges)
-		std::cout << num << " ";
-	std::cout << std::endl << std::endl;
-
-	Grid::Edge edge = grid.GetEdge(1777);
-
-	for (const auto& v : edge.vertexes)
-		std::cout << v << " ";
-	std::cout << std::endl;
-	for (const auto& n : edge.elemNums)
-		std::cout << n << " ";
-	std::cout << std::endl << std::endl;
-
-	edge = grid.GetEdge(2044);
-
-	for (const auto& v : edge.vertexes)
-		std::cout << v << " ";
-	std::cout << std::endl;
-	for (const auto& n : edge.elemNums)
-		std::cout << n << " ";
-	std::cout << std::endl << std::endl;
-
-	edge = grid.GetEdge(2045);
-
-	for (const auto& v : edge.vertexes)
-		std::cout << v << " ";
-	std::cout << std::endl;
-	for (const auto& n : edge.elemNums)
-		std::cout << n << " ";
-	std::cout << std::endl << std::endl;
-
-	edge = grid.GetEdge(2046);
-
-	for (const auto& v : edge.vertexes)
-		std::cout << v << " ";
-	std::cout << std::endl;
-	for (const auto& n : edge.elemNums)
-		std::cout << n << " ";
-	std::cout << std::endl << std::endl;
-
-
 	grid.WriteGrid();
 
 	FEM fem(grid, [](const std::array<double, SIZE_NODE>& args)
